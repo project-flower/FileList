89e45ccb5e8e18d6719568d577debbf27d3b1030
Yuki Nishina <project_flower@outlook.com>
2022-01-29 21:24:53 +0900

    新規作成

---
+++ FileList/FormMain.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace FileList
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxMode = new System.Windows.Forms.GroupBox();
+            this.radioButtonFile = new System.Windows.Forms.RadioButton();
+            this.radioButtonClipboard = new System.Windows.Forms.RadioButton();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.groupBoxMode.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxMode
+            // 
+            this.groupBoxMode.Controls.Add(this.radioButtonFile);
+            this.groupBoxMode.Controls.Add(this.radioButtonClipboard);
+            this.groupBoxMode.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxMode.Name = "groupBoxMode";
+            this.groupBoxMode.Size = new System.Drawing.Size(83, 62);
+            this.groupBoxMode.TabIndex = 0;
+            this.groupBoxMode.TabStop = false;
+            this.groupBoxMode.Text = "&Mode";
+            // 
+            // radioButtonFile
+            // 
+            this.radioButtonFile.AutoSize = true;
+            this.radioButtonFile.Location = new System.Drawing.Point(6, 40);
+            this.radioButtonFile.Name = "radioButtonFile";
+            this.radioButtonFile.Size = new System.Drawing.Size(42, 16);
+            this.radioButtonFile.TabIndex = 1;
+            this.radioButtonFile.Text = "&File";
+            this.radioButtonFile.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonClipboard
+            // 
+            this.radioButtonClipboard.AutoSize = true;
+            this.radioButtonClipboard.Checked = true;
+            this.radioButtonClipboard.Location = new System.Drawing.Point(6, 18);
+            this.radioButtonClipboard.Name = "radioButtonClipboard";
+            this.radioButtonClipboard.Size = new System.Drawing.Size(71, 16);
+            this.radioButtonClipboard.TabIndex = 0;
+            this.radioButtonClipboard.TabStop = true;
+            this.radioButtonClipboard.Text = "&Clipboard";
+            this.radioButtonClipboard.UseVisualStyleBackColor = true;
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "テキスト文書|*.txt|すべてのファイル|*.*";
+            // 
+            // FormMain
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(107, 86);
+            this.Controls.Add(this.groupBoxMode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormMain";
+            this.Text = "FileList";
+            this.TopMost = true;
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.dragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.dragEnter);
+            this.groupBoxMode.ResumeLayout(false);
+            this.groupBoxMode.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxMode;
+        private System.Windows.Forms.RadioButton radioButtonFile;
+        private System.Windows.Forms.RadioButton radioButtonClipboard;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ FileList/FormMain.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FileList
+{
+    public partial class FormMain : Form
+    {
+        #region private_fields
+
+        private bool messageShowing = false;
+
+        #endregion
+
+        #region private_methods
+
+        private void saveFile(string text)
+        {
+            if (messageShowing)
+            {
+                return;
+            }
+
+            messageShowing = true;
+            DialogResult dialogResult = saveFileDialog.ShowDialog(this);
+            messageShowing = false;
+
+            if (dialogResult != DialogResult.OK)
+            {
+                return;
+            }
+
+            try
+            {
+                File.WriteAllText(saveFileDialog.FileName, text);
+            }
+            catch (Exception exception)
+            {
+                showErrorMessage(exception.Message);
+            }
+        }
+
+        private void setClipboard(string text)
+        {
+            try
+            {
+                Clipboard.SetText(text);
+                showMessage("クリップボードにコピーしました。", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch(Exception exception)
+            {
+                showErrorMessage(exception.Message);
+            }
+        }
+
+        private void showErrorMessage(string message)
+        {
+            showMessage(message, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private DialogResult showMessage(string text, MessageBoxButtons buttons, MessageBoxIcon icon)
+        {
+            if (messageShowing)
+            {
+                return DialogResult.None;
+            }
+
+            messageShowing = true;
+            DialogResult result = MessageBox.Show(this, text, Text, buttons, icon);
+            messageShowing = false;
+            return result;
+        }
+
+        #endregion
+
+        // designer_methods
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void dragDrop(object sender, DragEventArgs e)
+        {
+            var dropData = (e.Data.GetData(DataFormats.FileDrop)) as string[];
+
+            if ((dropData == null) || (dropData.Length < 1))
+            {
+                return;
+            }
+
+            string newLine = System.Environment.NewLine;
+            string text = (string.Join(newLine, dropData, 0, dropData.Length) + newLine);
+            Delegate method = null;
+
+            if (radioButtonClipboard.Checked)
+            {
+                method = new Action<string>(setClipboard);
+            }
+
+            if (radioButtonFile.Checked)
+            {
+                method = new Action<string>(saveFile);
+            }
+
+            if (method == null)
+            {
+                return;
+            }
+
+            BeginInvoke(method, text);
+        }
+
+        private void dragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect
+                = (e.Data.GetDataPresent(DataFormats.FileDrop)
+                ? DragDropEffects.All
+                : DragDropEffects.None);
+        }
+    }
+}
